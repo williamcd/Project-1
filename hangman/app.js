c566e848bbf945e9aef75b33a9a4f60fda84350d
williamcd <wcdetrick@me.com>
2018-02-05 15:25:49 -0500

    updated hangman.js

--- hangman/app.js
+++ hangman/app.js
@@ -1,7 +1,10 @@
 const words = ['Banana', 'Strawberry', 'Mango', 'Peach', ];
 let word = [];
+let spaces;
 
 function newGame() {
-    word = words[Math.floor(Math.random()*words.length)];
+    word = (words[Math.floor(Math.random()*words.length)]).split("");
+    spaces = word.length;
     console.log(word);
+    console.log(spaces);
 }
