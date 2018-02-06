c594a62f4ca7b15ce2d3f6d1062961438c6ccc9a
williamcd <wcdetrick@me.com>
2018-02-06 10:25:23 -0500

    edited hangman app.js/style.css

--- hangman/app.js
+++ hangman/app.js
@@ -1,4 +1,5 @@
-const words = ['Banana', 'Strawberry', 'Mango', 'Peach', ];
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const words = ['banana', 'strawberry', 'mango', 'peach', ];
 let word = [];
 let spaces;
 
@@ -8,3 +9,15 @@ function newGame() {
     console.log(word);
     console.log(spaces);
 }
+
+let buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+    myButtons.append(letters);
+
+    for (i = 0; i < alphabet.length; i++) {
+        letter = document.createElement('li');
+        letters.append(letter);
+    }
+}
+buttons()
